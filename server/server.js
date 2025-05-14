6085eadae72230d7964354b27ccba758b653222a
Satyam kakra <satyamkakra4u@gmail.com>
2025-05-14 16:06:25 +0530

    project completed

--- server/server.js
+++ server/server.js
@@ -20,8 +20,7 @@ await connectDB();
 await connectCloudinary()
 
 // allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
-
+const allowedOrigins = ['http://localhost:5173', 'https://e-commerce-cya1.vercel.app']
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
 // Middleware configuration
